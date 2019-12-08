0fe14a3b5bf7da61ddc672ede7c2aef8ff14c5c7
jisan25 <shihabunjisan@gmail.com>
2019-12-09 03:26:21 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.scss';
+
+import Navbar from './components/navbar/Navbar';
+import TourList from './components/TourList';
+
+function App() {
+  return (
+    
+      <main>
+          <Navbar/>
+          <TourList/>
+      </main>
+      
+ 
+  );
+}
+
+export default App;
